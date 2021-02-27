caf53b76862fbbf99ce7a8b4dc0efa80a2df8f4b
VitaliyLavrov <LavroWitaliy@gmail.com>
2021-02-27 14:22:05 +0200

    Phonebook

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -50,23 +50,19 @@ public class Main {
         // Найти и вывести список уникальных слов, из которых состоит массив (дубликаты не считаем).
         // Посчитать, сколько раз встречается каждое слово.
 
-       System.out.println("слова"+ "\t повторения");
-      for (Map.Entry<String, Integer> entry : strMap.entrySet()) {
-         System.out.println(entry.getKey() +"\t\t"+ entry.getValue());
-       }
+        System.out.println("слова" + "\t повторения");
+        for (Map.Entry<String, Integer> entry : strMap.entrySet()) {
+            System.out.println(entry.getKey() + "\t\t" + entry.getValue());
+        }
 
 
         System.out.println("\n\n\n\n\n\n");
 
 
-
-
-
-
-
         Phonebook phonebook = new Phonebook();
-        phonebook.setNewNumber("Абрамова Екатерина Артёмовна", 1);
-        phonebook.setNewNumber("Агеев Фёдор Васильевич", 2);
+
+        phonebook.setNewNumber("Абрамова Екатерина Артёмовна", 145);
+        phonebook.setNewNumber("Агеев Фёдор Васильевич", 2465);
         phonebook.setNewNumber("Алексеев Владимир Даниилович", 4454963);
         phonebook.setNewNumber("Алешина Ярослава Романовна", 45487963);
         phonebook.setNewNumber("Антонова Мария Ярославовна", 6587963);
@@ -86,7 +82,6 @@ public class Main {
         phonebook.setNewNumber("Литвинов Всеволод Александрович", 765487963);
         phonebook.setNewNumber("Лавров Артемий Владимирович", 865487963);
         phonebook.setNewNumber("Лавров Артемий Владимирович", 865487963);
-
 
 
         phonebook.getNumber("Антонова Мария Ярославовна");
--- src/com/company/Phonebook.java
+++ src/com/company/Phonebook.java
@@ -11,70 +11,61 @@ public class Phonebook {
 
     ArrayList<Entry> phonebook;
 
+    //  Написать простой класс Телефонный Справочник, который хранит в себе список фамилий и телефонных номеров
     public Phonebook() {
         this.phonebook = new ArrayList<>();
     }
 
-
-    public void setNewNumber (String name, long number){
+    //В этот телефонный справочник с помощью метода можно добавлять записи
+    public void setNewNumber(String name, long number) {
         phonebook.add(new Entry(number, name));
 
 
     }
 
-    public void getNumber(String name){
+    //а с помощью метода get() искать номер телефона по фамилии
+    public void getNumber(String name) {
         for (Entry entry : phonebook) {
-        if (entry.name == name)
-            System.out.println(entry);
+            if (entry.name == name)
+                System.out.println(entry);
         }
 
 
-
     }
 
+    private class Entry {
+        long number;
+        String name;
 
+        public long getNumber() {
+            return number;
+        }
 
+        public void setNumber(long number) {
+            this.number = number;
+        }
 
+        public String getName() {
+            return name;
+        }
 
+        public void setName(String name) {
+            this.name = name;
+        }
 
-
-    private class Entry{
-    long number;
-    String name;
-
-    public long getNumber() {
-        return number;
-    }
-
-    public void setNumber(long number) {
-        this.number = number;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Entry(long number, String name) {
-        this.number = number;
-        this.name = name;
-    }
+        public Entry(long number, String name) {
+            this.number = number;
+            this.name = name;
+        }
 
         @Override
         public String toString() {
             return
 
-                    name + "  "+ this.number;
+                    name + "  " + this.number;
 
         }
     }
-
-
-
-
 
 
 }
